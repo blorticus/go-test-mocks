03c57e6697ab430388249d919fad55e2d1a65d21
blorticus <boguese@hotmail.com>
2021-10-28 00:57:02 -0700

    added godoc

--- reader.go
+++ reader.go
@@ -1,3 +1,4 @@
+// Package mock provides mock objects that can be used to make golang testing easier.
 package mock
 
 import "io"
@@ -8,12 +9,19 @@ type readerResult struct {
 	err                     error
 }
 
+// Reader is a mock io.Reader.  It is primed with values that will be extracted with each
+// successive Read().  It can be primed with a GoodRead (meaning no error is returned and
+// at least some bytes are present), an EmptyRead (meaning no error is returned, but no
+// bytes are also returned), an Error (meaning an error other than io.EOF is returned) or
+// EOF (meaning io.EOF is returned).  Once EOF is reached, it will be returned on all
+// subsequent calls to Read().
 type Reader struct {
 	readResults        []*readerResult
 	nextResultToReturn int
 	atEOF              bool
 }
 
+// NewReader create a new, empty mock Reader.
 func NewReader() *Reader {
 	return &Reader{
 		readResults:        make([]*readerResult, 0, 20),
@@ -22,6 +30,7 @@ func NewReader() *Reader {
 	}
 }
 
+// AddGoodRead adds a GoodRead to the queue of return values.
 func (reader *Reader) AddGoodRead(bytesToCopyToCallerBuffer []byte) *Reader {
 	reader.readResults = append(reader.readResults, &readerResult{
 		bytesToCopyToReadBuffer: bytesToCopyToCallerBuffer,
@@ -31,6 +40,7 @@ func (reader *Reader) AddGoodRead(bytesToCopyToCallerBuffer []byte) *Reader {
 	return reader
 }
 
+// AddEmptyRead adds an EmptyRead to the queue of return values.
 func (reader *Reader) AddEmptyRead() *Reader {
 	reader.readResults = append(reader.readResults, &readerResult{
 		bytesToCopyToReadBuffer: []byte{},
@@ -40,6 +50,7 @@ func (reader *Reader) AddEmptyRead() *Reader {
 	return reader
 }
 
+// AddError adds an Error to the queue of return values.
 func (reader *Reader) AddError(err error) *Reader {
 	reader.readResults = append(reader.readResults, &readerResult{
 		bytesToCopyToReadBuffer: []byte{},
@@ -49,6 +60,7 @@ func (reader *Reader) AddError(err error) *Reader {
 	return reader
 }
 
+// AddEOF adds EOF to the queue of return values.
 func (reader *Reader) AddEOF() *Reader {
 	reader.readResults = append(reader.readResults, &readerResult{
 		bytesToCopyToReadBuffer: []byte{},
@@ -59,6 +71,7 @@ func (reader *Reader) AddEOF() *Reader {
 	return reader
 }
 
+// Read implements the io.Reader Read() method.
 func (reader *Reader) Read(b []byte) (n int, err error) {
 	if reader.atEOF || len(reader.readResults) == 0 {
 		return 0, io.EOF
